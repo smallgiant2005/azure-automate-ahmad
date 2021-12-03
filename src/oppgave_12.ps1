f73f4965ffc1e666b45d082440e4ef176b84f161
Sharifinejad <ahmad.sharifinejad@nav.no>
2021-12-03 11:55:48 +0100

    oppgave12.ps1

---
+++ src/oppgave_12.ps1
@@ -0,0 +1,3 @@
+3 ressurser ble slettet manuelle.
+For å kunne slette alle ressurser så må man stå inne i sin sandbox Resource Group i portalen.
+Marker alle ressurser som skal slettes (3 stk) og trykk på Delete oppe til høyre.
